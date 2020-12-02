e4302610f744d98d1090f3fabfb18cb3f7028bc5
Raúl <raulguzmanguerrero@outlook.com>
2020-12-02 01:32:03 -0600

    Added table in home

--- static/js/home.js
+++ static/js/home.js
@@ -10,10 +10,10 @@ window.onload = () => {
     if (!token) {
         logout();
     } else {
-        headers = {Authorization: `bearer ${token}`,}
+        headers = { Authorization: `bearer ${token}` };
 
         let decoded = jwt_decode(token);
-        sessionStorage.setItem('page', 0);
+        sessionStorage.setItem("page", 0);
 
         let username = document.querySelector("#username");
         if (decoded.nombre && decoded.apellido) {
@@ -34,14 +34,67 @@ function logout() {
 async function getEmployees() {
     const response = await fetch(`${url}/empleados`, {
         method: "GET",
-        headers: headers
+        headers: headers,
     });
 
     const json = await response.json();
+    let table = employeesTable(json["message"]["results"]);
 
-    console.log(json)
+    document.querySelector("#table-container").appendChild(table);
 }
 
-function employeesTable(employees){
-    
+function employeesTable(employees) {
+    let table = document.createElement("table");
+    table.classList.add("table", "table-bordered", "table-hover", "table-sm", "table-striped");
+
+    let columnNames = ["#", "Nombre", "Apellidos", "Teléfono", "Correo", "Dirección"];
+
+    let head = document.createElement("thead");
+    let headerRow = document.createElement("tr");
+
+    columnNames.forEach((element) => {
+        let header = document.createElement("th");
+        header.setAttribute("scope", "col");
+        header.innerText = element;
+
+        headerRow.appendChild(header);
+    });
+
+    head.appendChild(headerRow);
+    table.appendChild(head);
+
+    let rows = [];
+    let counter = 1;
+
+    employees.forEach((element) => {
+        let keys = Object.keys(element);
+        let row = document.createElement("tr");
+
+        for (let i = 0; i < keys.length; i++) {
+            let column;
+
+            if (keys[i] == "id") {
+                column = document.createElement("th");
+                column.setAttribute("scope", "row");
+                column.innerText = counter;
+            } else {
+                column = document.createElement("td");
+                column.innerText = element[keys[i]] || "-";
+            }
+
+            row.appendChild(column);
+        }
+
+        rows.push(row);
+        counter++;
+    });
+
+    let tableBody = document.createElement("tbody");
+    rows.forEach((element) => {
+        tableBody.appendChild(element);
+    });
+
+    table.appendChild(tableBody);
+
+    return table;
 }
